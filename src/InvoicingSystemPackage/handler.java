acaa04caa3d01e4f0fd50a310be72f19cf8ff18d
kaizen-98 <ammalnassri@gmail.com>
2023-03-08 23:46:45 -0800

    add handler for the items

--- src/InvoicingSystemPackage/handler.java
+++ src/InvoicingSystemPackage/handler.java
@@ -31,11 +31,12 @@ public class handler {
 			String shop_name = input.next();
 
 			String inserShop = "insert into shop_settings (id,shop_name) values(" + id + ",'" + shop_name + "')";
-			System.out.println("Enter The Shop Id" + id);
-			System.out.println("Enter The Shop Name" + shop_name);
-
-			st.execute(inserShop);
-
+			Integer m = st.executeUpdate(inserShop);
+            if (m >= 1) {
+            	System.out.println("inserted successfully : ");
+} else {
+System.out.println("insertion failed");
+}
 			// -----------------------------------------------------------//
 
 			con.close();
@@ -43,5 +44,45 @@ public class handler {
 			System.err.println(ex);
 		}
 
+	}
+	
+	public void addItems() {
+
+		String url = "jdbc:sqlserver://localhost:1433;" + "databaseName=InvoicingSystemDB;" + "encrypt=true;"
+				+ "trustServerCertificate=true";
+		String user = "sa";
+		String pass = "root";
+
+		Connection con = null;
+		try {
+			Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+			DriverManager.registerDriver(driver);
+			con = DriverManager.getConnection(url, user, pass);
+			Statement st = con.createStatement();
+			// -------------------------------------------------------------//
+
+			System.out.println("Enter The item Id");
+			int id = input.nextInt();
+			System.out.println("Enter The item Name");
+			String item_name = input.next();
+			System.out.println("write a description");
+			String des = input.next();
+			System.out.println("Enter The price");
+			int price = input.nextInt();
+			
+			
+			String inserShop = "insert into shop_items(id,name,description,price) values(" + id + ",'" + item_name+"','"+des+"',"+price+ ")";
+			Integer m = st.executeUpdate(inserShop);
+            if (m >= 1) {
+            	System.out.println("inserted successfully !! ");
+} else {
+System.out.println("insertion failed");
+}
+			// -----------------------------------------------------------//
+
+			con.close();
+		} catch (Exception ex) {
+			System.err.println(ex);
+		}
 	}
 }
