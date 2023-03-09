1837bace140aaef8188f3367d4cc94adabb4768b
kaizen-98 <ammalnassri@gmail.com>
2023-03-09 03:23:46 -0800

    add heandler for inovice and inoviceitem

--- src/InvoicingSystemPackage/handler.java
+++ src/InvoicingSystemPackage/handler.java
@@ -85,4 +85,80 @@ System.out.println("insertion failed");
 			System.err.println(ex);
 		}
 	}
+	
+	public void addinvoice_items() {
+		String url = "jdbc:sqlserver://localhost:1433;" + "databaseName=InvoicingSystemDB;" + "encrypt=true;"
+				+ "trustServerCertificate=true";
+		String user = "sa";
+		String pass = "root";
+
+		Connection con = null;
+		try {
+			Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+			DriverManager.registerDriver(driver);
+			con = DriverManager.getConnection(url, user, pass);
+			Statement st = con.createStatement();
+			// -------------------------------------------------------------//
+
+			System.out.println("Enter The invoice Id");
+			int id = input.nextInt();
+			System.out.println("Enter The item Id");
+			int idItem = input.nextInt();
+			System.out.println("Enter The quantity");
+			int quantity = input.nextInt();
+			System.out.println("Enter The price");
+			int price = input.nextInt();
+			
+			
+			String inserInvoice = "insert into invoice_items(invoice_id,item_id,quantity,price) values(" + id + ","+idItem+","+quantity+","+price+ ")";
+			Integer m = st.executeUpdate(inserInvoice);
+            if (m >= 1) {
+            	System.out.println("inserted successfully !! ");
+} else {
+System.out.println("insertion failed");
+}
+			// -----------------------------------------------------------//
+
+			con.close();
+		} catch (Exception ex) {
+			System.err.println(ex);
+		}
+	}
+	public void addInvoice() {
+		String url = "jdbc:sqlserver://localhost:1433;" + "databaseName=InvoicingSystemDB;" + "encrypt=true;"
+				+ "trustServerCertificate=true";
+		String user = "sa";
+		String pass = "root";
+
+		Connection con = null;
+		try {
+			Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+			DriverManager.registerDriver(driver);
+			con = DriverManager.getConnection(url, user, pass);
+			Statement st = con.createStatement();
+			// -------------------------------------------------------------//
+
+			System.out.println("Enter The Invoice Id");
+			int id = input.nextInt();
+			System.out.println("Enter Invoice date");
+			String Invoice_date = input.next();
+			System.out.println("write a custmer name?");
+			String Cname = input.next();
+			
+			
+			
+			String inserInvoice = "insert into invoices (id,invoice_date,customer_name) values(" + id + ",'"+Invoice_date+"','"+Cname+"')";
+			Integer m = st.executeUpdate(inserInvoice);
+            if (m >= 1) {
+            	System.out.println("inserted successfully !! ");
+} else {
+System.out.println("insertion failed");
+}
+			// -----------------------------------------------------------//
+
+			con.close();
+		} catch (Exception ex) {
+			System.err.println(ex);
+		}
+	}
 }
--- src/InvoicingSystemPackage/test.java
+++ src/InvoicingSystemPackage/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		handler handler = new handler();
 		
-		handler.addItems();
+		handler.addinvoice_items();
 	}
 
 }
